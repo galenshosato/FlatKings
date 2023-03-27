3c79d24d926b18dfb75e659ac36814e97cb4218c
Galen Sho Sato <galen.sato@gmail.com>
2023-03-27 13:24:55 -0400

    Finished basic models with to_dict methods

--- server/models.py
+++ server/models.py
@@ -8,3 +8,48 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    user_bets = db.relationship('UserBet', backref='user')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password
+        }
+    
+class Bet(db.Model):
+    __tablename__= "bets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_name = db.Column(db.String)
+    desc = db.Column(db.String)
+    odds = db.Column(db.Integer)
+    wager = db.Column(db.Integer)
+    success = db.Column(db.Boolean)
+    user_bets = db.relationship('UserBet', backref='bet')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "team name": self.team_name,
+            "description": self.desc,
+            "odds": self.odds,
+            "wager": self.wager,
+            "W/L": self.success,
+        }
+
+class UserBet(db.Model):
+    __tablename__ = 'user_bets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    bet_id = db.Column(db.Integer, db.ForeignKey('bets.id'))
+
